e136b16631aca05f8008beb5b92d128faabdb9d7
lovesicknd <sickndlove@gmail.com>
2025-04-18 14:59:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ import requests
 import websocket
 from keep_alive import keep_alive
 
-status = "online" #online/dnd/idle
+status = "dnd" #online/dnd/idle
 
-GUILD_ID = ADD_YOUR_SERVER_ID_HERE
-CHANNEL_ID = ADD_YOUR_CHANNEL_ID_HERE
-SELF_MUTE = True
+GUILD_ID = 1168862491099549736
+CHANNEL_ID = 1362721694422401114
+SELF_MUTE = False
 SELF_DEAF = False
 
 usertoken = os.getenv("TOKEN")
